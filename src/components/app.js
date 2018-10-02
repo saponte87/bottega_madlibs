9e873f20391c851276526db1d72130e6bf081adb
Stephen Aponte <saponte1987@gmail.com>
2018-10-02 15:33:06 -0600

    changed app title

--- src/components/app.js
+++ src/components/app.js
@@ -3,9 +3,8 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Bootstrap</h2>
+      <div>
+        <h1>Bottega Madlibs</h1>
       </div>
     );
   }
