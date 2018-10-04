94bbb7e49248b0bc9984db4134207bedfddc60fc
Stephen Aponte <saponte1987@gmail.com>
2018-10-04 13:32:48 -0600

    re-positioned the generate button, and fixed content counter for label titles

--- src/components/content.js
+++ src/components/content.js
@@ -6,6 +6,11 @@ class Content extends Component {
             super()
             this.count = 1;
     }
+
+    componentWillReceiveProps() {
+        this.count = 1;
+    }
+
     renderLabel(data){
         return [
             <div className="card__content__label">{this.count++}</div>,
